590e1dc70ae8a860556ae436dbacf995d7f893d8
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-09-23 17:19:16 +0200

    implementig the DOM

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-        let userName = prompt(`What's your name?`);
         let answer = Math.floor(Math.random() * 100) + 1;
-        let guess;
         console.log(answer);
 
         function game (answer, playerName) {
-            guess = +prompt(`What's your answer?`);
-            if (typeof guess === NaN){
-                prompt('Invalid entry');
-                game(answer, playerName);
-            }else{
+             playerName = userName;
+            console.log(userName);
+            clue.innerHTML = '';
+            let guess = +prompt('Enter a value');
+            // if (typeof guess === NaN){
+            //     prompt('Invalid entry');
+            //     game(answer, playerName);
+            // }else{
                 if (guess === answer){
                     console.log(`${playerName} wins!`);
+                    answerDisplay.innerHTML = answer;
                     }else if (guess < answer){
-                        console.log('It is more');
+                        clue.innerHTML = 'It is more';
                         game(answer, playerName);
                     }else if (guess > answer){
-                        console.log('It is less');
+                        clue.innerHTML = 'It is less';
                         game(answer, playerName);
                     }     
-            }         
-         }
-        game(answer, userName);
+            // }         
+        }
 
+
+        
+    const userName = document.querySelector('.name-input').value;
+    const playButton = document.querySelector('.play-button');
+    const clue = document.querySelector('.high-low-indicator');
+    let userGuess = document.querySelector('.number-input').value;
+    const answerDisplay = document.querySelector('.answer');
+        
+        
+        
+        
+    playButton.addEventListener('click', () => {
+        game(answer, userName);
+    });
